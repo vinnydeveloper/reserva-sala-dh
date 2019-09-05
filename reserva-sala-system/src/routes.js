d2789d9a33302b5515caa983d96c92d1d5493315
Vinicius Oliveira <vinicius.developer@outlook.com>
2019-09-05 17:54:18 -0300

    insert pages login and register

--- reserva-sala-system/src/routes.js
+++ reserva-sala-system/src/routes.js
@@ -7,5 +7,13 @@ routes.get('/', (req, res) => res.render('index', {
   layout: 'default',
 }));
 
+routes.get('/login', (req, res) => res.render('login', {
+  layout: 'auth',
+}));
+
+routes.get('/registro', (req, res) => res.render('registro', {
+  layout: 'auth',
+}));
+
 
 module.exports = routes;
