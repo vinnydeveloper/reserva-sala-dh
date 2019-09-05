1fa82869420f10882f65173a39f107c4f05fb2ad
Vinicius Oliveira <vinicius.developer@outlook.com>
2019-09-05 15:54:04 -0300

    incluindo a funcionalidade de template

--- reserva-sala-system/src/routes.js
+++ reserva-sala-system/src/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
+
 const routes = express.Router();
 
-//rotas padrãoes sistema
-routes.get('/', (req,res)=>{
-    return res.render('index')
+// rotas padrãoes sistema
+routes.get('/', (req, res) => {
+  return res.render('index', { layout: 'default' });
+});
 
-})
-
-module.exports = routes
+module.exports = routes;
--- reserva-sala-system/src/server.js
+++ reserva-sala-system/src/server.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 const routes = require('./routes');
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  hbs({
+    extname: 'hbs',
+    defaultView: 'default',
+    layoutsDir: `${__dirname}/views/layouts/`,
+  })
+);
 app.use(routes);
 app.listen(3300, console.log('Servidor rodando na porta 3300'));
