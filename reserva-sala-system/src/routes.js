51a3cbe748c5081b5f8122e9e8d2fe20a92ec2bf
fronthendy <hendy.almeida@hotmail.com>
2019-09-19 18:05:38 -0300

    pagina calendario

--- reserva-sala-system/src/routes.js
+++ reserva-sala-system/src/routes.js
@@ -9,8 +9,7 @@ const Auth = require('./app/middlewares/auth');
 // rotas padrãoes sistema
 routes.get('/', Auth.verificarLogin, (req, res) => res.render('index'));
 
-routes.get('/login', (req, res) => res.render('login', {
-}));
+routes.get('/login', (req, res) => res.render('login', {}));
 routes.post('/login', LoginController.logar);
 routes.get('/logout', LoginController.deslogar);
 
@@ -20,9 +19,22 @@ routes.post('/usuario/cadastrar', UsuarioController.cadastrar);
 
 
 routes.get('/reserva', Auth.verificarLogin, (req, res) => {
-  const { session } = req;
+  const {
+    session
+  } = req;
 
   return res.render('reserva', {
+    logado: true,
+    nome: session.usuario.nome,
+  });
+});
+
+routes.get('/calendario', Auth.verificarLogin, (req, res) => {
+  const {
+    session
+  } = req;
+
+  return res.render('calendario', {
     logado: true,
     nome: session.usuario.nome,
   });
--- reserva-sala-system/src/server.js
+++ reserva-sala-system/src/server.js
@@ -2,17 +2,39 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const hbs_sections = require('express-handlebars-sections');
 const session = require('express-session');
 const routes = require('./routes');
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(session({ secret: 'reserva@123', resave: true, saveUninitialized: true }));
+app.use(session({
+  secret: 'reserva@123',
+  resave: true,
+  saveUninitialized: true
+}));
 app.use('/', express.static(`${__dirname}/public`));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: `${__dirname}/views/layouts/`, defaultLayout: 'default' }));
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  layoutsDir: `${__dirname}/views/layouts/`,
+  section: hbs_sections(),
+  defaultLayout: 'default',
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) {
+        this._sections = {}
+      };
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+}));
+
 app.use((req, res, next) => {
   res.locals.session = req.session;
   next();
